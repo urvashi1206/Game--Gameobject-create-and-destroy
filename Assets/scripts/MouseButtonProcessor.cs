ebe4c21b2167c9030b98da0bfcc0b8b935d8bb5d
Urvashi Dhingra <dhingraurvashi99@gmail.com>
2020-07-01 21:56:59 +0530

    game three

---
+++ Assets/scripts/MouseButtonProcessor.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Processes mouse button inputs
+/// </summary>
+public class MouseButtonProcessor : MonoBehaviour
+{
+    [SerializeField]
+    GameObject prefabExplosion;
+    [SerializeField]
+    GameObject prefabTeddyBear;
+
+    GameObject character;
+
+    // first frame input support
+    bool spawnInputOnPreviousFrame = false;
+	bool explodeInputOnPreviousFrame = false;
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        // spawn teddy bear as appropriate
+        if (Input.GetAxis("SpawnTeddyBear") > 0)
+        {
+            if (!spawnInputOnPreviousFrame)
+            {
+                spawnInputOnPreviousFrame = true;
+                Vector3 position = Input.mousePosition;
+                position.z = -Camera.main.transform.position.z;
+                position = Camera.main.ScreenToWorldPoint(position);
+                GameObject.Instantiate(prefabTeddyBear, position, Quaternion.identity);
+            }
+        }
+        else
+        {
+            spawnInputOnPreviousFrame = false;
+        }
+        // explode teddy bear as appropriate
+        if (Input.GetAxis("ExplodeTeddyBear") > 0)
+        {
+            if (!explodeInputOnPreviousFrame)
+            {
+                explodeInputOnPreviousFrame = true;
+                GameObject teddyBear = GameObject.FindGameObjectWithTag("TeddyBear");
+                if (teddyBear != null) ;
+                {
+                    Instantiate(prefabExplosion, teddyBear.transform.position, Quaternion.identity);
+                    Destroy(teddyBear);
+                }
+            }
+        }
+        else
+        {
+            explodeInputOnPreviousFrame = false;
+        }
+    }
+}
